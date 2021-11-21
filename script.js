1f54cd3da73ab6a02cb180b8f4b8b680001713e4
KryptonANSHU <anshu191001@gmail.com>
2021-11-21 18:51:39 +0530

    Changes

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-var a=prompt("Enter UserName");
- var b=prompt("Enter password");
-  	if(a== "Eman Sabir" && b=="27-06-1996")
-  	{
-  	    window.location.href="start.html";
-  	}
-  	else
-  	{
-  		alert("invalid details");
-  		window.location.href="index.html";
-  	}
+
   	
+
+	  const panels = document.querySelectorAll('.panel');
+	  const btn = document.getElementById('btn');
+	  const con = document.getElementById('container');
+	  // console.log(panels)
+	  
+	  panels.forEach((element)=>{
+		  element.addEventListener('click',()=>{
+			  con.classList.remove('set');;
+			  removeactive();
+			  element.classList.add('active');
+			 
+		  })
+	  })
+	  
+	  btn.addEventListener('click',()=>{
+		  panels.forEach((ele)=>{
+			  ele.classList.remove('active');  
+			  con.classList.add('set');
+		  })
+	  })
+	  
+		  function removeactive(){
+			  panels.forEach((element)=>{
+				  element.classList.remove('active');
+			  })
+		  }
